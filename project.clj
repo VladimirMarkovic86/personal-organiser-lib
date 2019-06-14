c00b47e11e0438faba9fe79315eea66161bad119
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-14 14:28:12 +0200

    Task 64: Implemented card view in html and latex reports, preferences window is available also.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/personal-organiser-lib "0.1.5"
+(defproject org.clojars.vladimirmarkovic86/personal-organiser-lib "0.1.6"
   :description "Personal organiser library"
   :url "http://github.com/VladimirMarkovic86/personal-organiser-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/personal-organiser-middle "0.2.8"]
+                 [org.clojars.vladimirmarkovic86/personal-organiser-middle "0.2.9"]
                  ]
 
   :min-lein-version "2.0.0"
